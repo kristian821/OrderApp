f5cbed6b5c3450081ff29977b3120c0138ecc35b
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2021-12-21 20:57:29 -0800

    Initial Commit

---
+++ OrderApp/OrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppApp.swift
+//  OrderApp
+//
+//  Created by Kristian Mitchell on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
